bbfa00b9f50f7e830253d7e6d8ecc856cafcb5bd
ErikBurdett <erikaburdet@gmail.com>
2021-06-18 11:40:26 -0500

    middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization
 
-    1- On valid token in the Authorization header, call next.
+  if(token){
+    jwt.verify(token, "keep it secret", (err, decoded)=>{
+      if (!err){
+        req.decodedToken = decoded;
+        next();
+        return;
+      }
+      res.status(401).json({
+        message: "token invalid"
+      })
+      return;
+    })
+    return;
+}
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+res.status(401).json({message: "token required"})
 };
